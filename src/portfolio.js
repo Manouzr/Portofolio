bf6ec6997b946d88f923cc34829d7f39c03d915d
Manou <76950179+Manouzr@users.noreply.github.com>
2023-01-19 15:49:20 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -119,15 +119,15 @@ const techStack = {
   experience: [
     {
       Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
+      progressPercentage: "60%" //Insert relative proficiency in percentage
     },
     {
-      Stack: "Backend",
+      Stack: "Scraping",
       progressPercentage: "70%"
     },
     {
-      Stack: "Programming",
-      progressPercentage: "60%"
+      Stack: "Scripting",
+      progressPercentage: "80%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
